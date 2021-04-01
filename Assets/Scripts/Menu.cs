a19f406d8e350310d0f9898d729df696b0bdf70c
dhukke <dhukke@gmail.com>
2021-04-01 14:56:54 -0300

    added main menu and success layers

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
