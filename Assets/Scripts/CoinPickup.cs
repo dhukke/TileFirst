edb332f4b7ddf597c9777e5b1894590f2596e3f3
dhukke <dhukke@gmail.com>
2021-04-02 17:20:44 -0300

    added coin sound fx

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] private AudioClip coinPickUpSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
